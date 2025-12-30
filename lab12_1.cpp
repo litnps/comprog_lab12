170011af64be26c04baed934a40a3dbe31efde8b
litnps <lalitth21.n@gmail.com>
2025-12-30 14:38:22 +0700

    (Question 2) lab12_1.cpp

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,17 @@
 #include<iostream>
+#include<ctime>
 using namespace std;
+
+long long int fibonacci(int);
 
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+long long int fibonacci(int i){
+    if(i == 0 || i == 1) return i;
+    return fibonacci(i-1) + fibonacci(i-2);
 }
